78df53b3e04d0c3ca6fb0cdc713300e03cec57a0
namaste-code <shitanshu.19p@gmail.com>
2019-05-19 23:47:32 +0530

    GraphQL Query & Mutation.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 
@@ -15,6 +16,11 @@ const schema = gql`
 
     messages: [Message!]!
     message(id: ID!): Message!
+  }
+
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
   }
 
   type User {
@@ -72,6 +78,34 @@ const resolvers = {
     },
     message: (parent, { id }) => {
       return messages[id];
+    }
+  },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id
+      };
+
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+
+      return message;
+    },
+
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
     }
   },
 
