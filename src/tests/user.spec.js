5f05aa3997a99a8123ee4d1fc6e15f6400dffb8c
namaste-code <shitanshu.19p@gmail.com>
2019-05-21 18:43:35 +0530

    Implementing E2E testing.

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,7 +1,57 @@
 import { expect } from 'chai';
 
+import * as userApi from './api';
+
 describe('users', () => {
-    it('user is user', () => {
-        expect('user').to.eql('user');
-    })
+    describe('user(id: String!): User', () => {
+        it('returns a user when user can be found', async () => {
+            const expectedResult = {
+                data: {
+                    user: {
+                        id: '1',
+                        username: 'shitanshu',
+                        email: 'hello@shitanshu.com',
+                        role: 'ADMIN'
+                    }
+                }
+            };
+
+            const result = await userApi.user({ id: '1' });
+
+            expect(result.data).to.eql(expectedResult);
+        });
+
+        it('returns null when user cannot be found', async () => {
+            const expectedResult = {
+                data: {
+                    user: null,
+                }
+            };
+
+            const result = await userApi.user({ id: '42' });
+
+            expect(result.data).to.eql(expectedResult);
+        });
+    });
+
+    describe('deleteUser(id: String!): Boolean!', () => {
+        it('returns an error because only admins can delete a user', async () => {
+            const {
+                data: {
+                    data: {
+                        signIn: { token },
+                    },
+                },
+            } = await userApi.signIn({
+                login: 'ddavid',
+                password: 'ddavidd',
+            });
+
+            const {
+                data: { errors },
+            } = await userApi.deleteUser({ id: '1' }, token);
+            
+            expect(errors[0].message).to.eql('Not authorized as admin.');
+        });
+    });
 });
