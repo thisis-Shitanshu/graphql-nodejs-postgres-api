6712c22a3ac0d749c169f8433ccddbf14863cd3e
namaste-code <shitanshu.19p@gmail.com>
2019-05-21 16:44:30 +0530

    Pagination and adding property to intermidiate layer.

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -6,6 +6,10 @@ import {
   isMessageOwner 
 } from './authorization';
 
+const toCursorHash = string => Buffer.from(string).toString('base64');
+
+const fromCursorHash = string => Buffer.from(string, 'base64').toString('ascii');
+
 export default {
   Query: {
     messages: async (
@@ -16,16 +20,29 @@ export default {
       const cursorOptions = cursor ? {
         where: {
           createdAt: {
-            [Sequelize.Op.lt]: cursor
+            [Sequelize.Op.lt]: fromCursorHash(cursor)
           }
         }
       } : {};
 
-      return await models.Message.findAll({
+      const messages = await models.Message.findAll({
         order: [['createdAt', 'DESC']],
-        limit,
+        limit: limit + 1,
         ...cursorOptions
       });
+
+      const hasNextPage = messages.length > limit;
+      const edges = hasNextPage ? messages.slice(0,-1) : messages;
+
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage,
+          endCursor: toCursorHash(
+            edges[edges.length - 1].createdAt.toString()
+          )
+        }
+      }
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findOne({
